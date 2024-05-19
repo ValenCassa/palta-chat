9e7020210789cf217d7a949dce00e430f9bf9078
Valentin Cassarino <valencassa@gmail.com>
2024-05-19 11:45:55 -0300

    test

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/auth/login", "/api/webhooks"]);
+const isPublicRoute = createRouteMatcher([
+  "/auth/login",
+  "/api/webhooks",
+  "/auth/login/sso-callback",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
